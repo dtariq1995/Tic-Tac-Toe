268d01bf503b94f130240f023620267267d8658c
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2022-11-15 14:18:39 -0600

    Added factory function for player gameboard module

---
+++ tictactoe.js
@@ -0,0 +1,12 @@
+
+
+const player = (name) => {   // this is a factory function used to create player objects
+    const sayHello = () => console.log('hello');
+    // const chooseSquare()   insert function that allows player to 
+    return { name, sayHello };
+};
+
+const Gameboard = (function() {
+
+
+})();
