ebba6643ce5710365b00681f9929d59234f0cd72
Daanyaal Tariq <77370572+dtariq1995@users.noreply.github.com>
2022-11-18 16:16:23 -0600

    Added functionality to control display

--- tictactoe.js
+++ tictactoe.js
@@ -10,10 +10,38 @@ const Gameboard = (function() {
 
     let gameboard = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
 
+    return {
+        gameboard
+    };
 })();
 
 
 const DisplayController = (function() {
 
+    let tl = document.getElementById("top-left");
+    tl.textContent = Gameboard.gameboard[0];
 
+    let tc = document.getElementById("top-center");
+    tc.textContent = Gameboard.gameboard[1];
+
+    let tr = document.getElementById("top-right");
+    tr.textContent = Gameboard.gameboard[2];
+
+    let ml = document.getElementById("middle-left");
+    ml.textContent = Gameboard.gameboard[3];
+
+    let mc = document.getElementById("middle-center");
+    mc.textContent = Gameboard.gameboard[4];
+
+    let mr = document.getElementById("middle-right");
+    mr.textContent = Gameboard.gameboard[5];
+
+    let bl = document.getElementById("bottom-left");
+    bl.textContent = Gameboard.gameboard[6];
+
+    let bc = document.getElementById("bottom-center");
+    bc.textContent = Gameboard.gameboard[7];
+
+    let br = document.getElementById("bottom-right");
+    br.textContent = Gameboard.gameboard[8];
 })();
