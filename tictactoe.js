3230da017f654b1fac1281754e455720122971ce
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2022-11-23 16:51:50 -0600

    Added functionality for users to choose square

--- tictactoe.js
+++ tictactoe.js
@@ -1,56 +1,29 @@
+// placeholder for special characters: Ｏ Ｘ
 
-
-const player = (name) => {   // this is a factory function used to create player objects
+const player = (name, mark) => {   // this is a factory function used to create player objects
     const sayHello = () => console.log('hello');
-    // const chooseSquare()   insert function that allows player to 
-    return { name, sayHello };
+    
+    return { name, mark };
 };
-
+ 
 const Gameboard = (function() {
 
-    let gameboard = ["Ｘ", "Ｏ", "Ｘ", "Ｏ", "Ｘ", "Ｏ", "Ｘ", "Ｏ", "Ｘ"];  // array that stores users choices
+    let gameboard = ["", "", "", "", "", "", "", "", ""];  // array that stores users choices
 
-
-    function chooseSquare() {
-
-        Gameboard.gameboard[1] = "Ｘ";
-        console.log(Gameboard.gameboard);
-    }
-
-
-    return { gameboard, chooseSquare };
+    return { gameboard };
 })();
 
 
-const DisplayController = (function() {  // displays gameboard array 
+const DisplayController = (function() {  // controls display
 
-    let tl = document.getElementById("top-left");
-    tl.textContent = Gameboard.gameboard[0];
+    function chooseSquare(square_id) {   // allows player to choose a square and stores choice in array
 
-    let tc = document.getElementById("top-center");
-    tc.textContent = Gameboard.gameboard[1];
+        let square = document.getElementById(square_id);
+        square.textContent = "X";
+        Gameboard.gameboard[square_id] = "X";
+        console.log(Gameboard.gameboard);
+    }
 
-    let tr = document.getElementById("top-right");
-    tr.textContent = Gameboard.gameboard[2];
-
-    let ml = document.getElementById("middle-left");
-    ml.textContent = Gameboard.gameboard[3];
-
-    let mc = document.getElementById("middle-center");
-    mc.textContent = Gameboard.gameboard[4];
-
-    let mr = document.getElementById("middle-right");
-    mr.textContent = Gameboard.gameboard[5];
-
-    let bl = document.getElementById("bottom-left");
-    bl.textContent = Gameboard.gameboard[6];
-
-    let bc = document.getElementById("bottom-center");
-    bc.textContent = Gameboard.gameboard[7];
-
-    let br = document.getElementById("bottom-right");
-    br.textContent = Gameboard.gameboard[8];
-
-    return { tl, tc, tr, ml, mc, mr, bl, bc, br };
+    return { chooseSquare };
 
 })();
